171b49dd18f0edc499c48860807bb9929feb3b26
Carlos Yuri Carranza Quispe <Yuri.Carranza@promart.pe>
2025-10-15 12:16:25 -0500

    initial commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    private final UserRepository _userRepository;
+    public UserController(UserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+    @GetMapping
+    public ResponseEntity<List<User>> getUsers(){
+        var users = _userRepository.GetUsers();
+        return ResponseEntity.ok(users);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") int id){
+        var user = _userRepository.GetUserById(id);
+        if (user.isPresent()) return ResponseEntity.ok(user.get());
+        else return ResponseEntity.notFound().build();
+    }
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        _userRepository.CreateUser(user);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+}
---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,35 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.User;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class UserRepository {
+    private final JdbcClient jdbcClient;
+    public UserRepository(JdbcClient jdbcClient){
+        this.jdbcClient = jdbcClient;
+    }
+    public List<User> GetUsers(){
+        var users = jdbcClient.sql("select id, name, email, phone from dbo.[User]").query(User.class).list();
+        return users;
+    }
+
+    public Optional<User> GetUserById(int id){
+        var user = jdbcClient.sql("select id, name, email, phone from dbo.[User] where id = :id")
+                .param("id", id)
+                .query(User.class).optional();
+        return user;
+    }
+
+    public void CreateUser(User user){
+        jdbcClient.sql("insert into dbo.[User](id, name, email, phone) values (:id, :name, :email, :phone)")
+                .param("id", user.getId())
+                .param("name", user.getName())
+                .param("email", user.getEmail())
+                .param("phone", user.getPhone()).update();
+    }
+}
